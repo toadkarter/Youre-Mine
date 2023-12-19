b229ef2bb86d0c991ecba8a9e088f7c84200a7f4
toadkarter <vrakhmanin1@gmail.com>
2023-12-19 18:25:57 +0000

    Hooks up player animations

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -4,13 +4,16 @@ extends CharacterBody2D
 @export var speed = 300.0
 @export var jump_velocity = -400.0
 
-
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	_handle_fall(delta)
 	_handle_jump_input()
 	_handle_move_input()
+	_handle_move_animations()
 	move_and_slide()
 
 
@@ -25,6 +28,17 @@ func _handle_move_input() -> void:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+
+
+func _handle_move_animations() -> void:
+	if velocity .x != 0 or velocity.y != 0:
+		animated_sprite.play("run")
+		if velocity.x > 0 and animated_sprite.flip_h:
+			animated_sprite.flip_h = false
+		elif velocity.x < 0 and !animated_sprite.flip_h:
+			animated_sprite.flip_h = true
+	else:
+		animated_sprite.play("default")
 
 
 func _handle_jump_input() -> void:
