a97265fd2424b46c69ee8901eec577402d1abcc2
toadkarter <vrakhmanin1@gmail.com>
2023-12-20 11:34:06 +0000

    Adding background to stinger text

--- Scenes/UI/hud.gd
+++ Scenes/UI/hud.gd
@@ -2,10 +2,11 @@ class_name HUD
 
 extends CanvasLayer
 
-@onready var stinger_label: Label = $StingerLabel
+@onready var stinger_background: ColorRect = $StingerBackground
+@onready var stinger_label: Label = $StingerBackground/StingerLabel
 @onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func show_stinger_text() -> void:
-	stinger_label.visible = true
+	stinger_background.visible = true
 	audio_stream_player.play()
