911d8591862b947d9cc8fd9b2729f8a1393cf9c9
toadkarter <vrakhmanin1@gmail.com>
2023-12-23 18:38:36 +0000

    Added movable platforms level

--- Scenes/Environment/Blocks/Disappearing/disappearing_block.gd
+++ Scenes/Environment/Blocks/Disappearing/disappearing_block.gd
@@ -2,12 +2,16 @@ extends StaticBody2D
 
 @export var seconds_before_start: float = 0.0
 @export var seconds_between_toggle: float = 5.0
+@export var start_visible: bool = true
 
 @onready var collider: CollisionShape2D = $Collider
 @onready var timer: Timer = $Timer
 
 
 func _ready():
+	if not start_visible:
+		_toggle()
+	
 	await get_tree().create_timer(seconds_before_start).timeout
 	timer.wait_time = seconds_between_toggle
 	timer.connect("timeout", _on_timeout)
@@ -15,5 +19,9 @@ func _ready():
 	
 	
 func _on_timeout():
+	_toggle()
+	
+	
+func _toggle():
 	visible = !visible
 	collider.disabled = !collider.disabled
