0a41cd357108e4c2a91db847dfde8d3b65481a6e
toadkarter <vrakhmanin1@gmail.com>
2023-12-26 12:29:28 +0000

    Added way for spikes to start retracted

--- Scenes/Environment/Damage/Spikes/Movable/movable_spikes.gd
+++ Scenes/Environment/Damage/Spikes/Movable/movable_spikes.gd
@@ -2,16 +2,20 @@ extends Node2D
 
 const SPIKES_EXTEND_ANIMATION_NAME: String = "spikes_extend"
 const SPIKES_RETRACT_ANIMATION_NAME: String = "spikes_retract"
+const RETRACTED_HEIGHT: float = 8.0
 
 @export var spikes_up_seconds: float = 1.0
 @export var spikes_down_seconds: float = 2.0
-
-var spikes_down: bool = false
+@export var start_retracted: bool = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var spikes: StaticBody2D = $Spikes
 
 
 func _ready() -> void:
+	if start_retracted:
+		spikes.position.y = RETRACTED_HEIGHT
+		await get_tree().create_timer(spikes_down_seconds).timeout
 	animation_player.connect("animation_finished", _on_animation_finished)
 	animation_player.play(SPIKES_EXTEND_ANIMATION_NAME)
 	
@@ -23,14 +27,3 @@ func _on_animation_finished(animation_name: String) -> void:
 	elif animation_name == SPIKES_EXTEND_ANIMATION_NAME:
 		await get_tree().create_timer(spikes_up_seconds).timeout
 		animation_player.play(SPIKES_RETRACT_ANIMATION_NAME)
-
-
-func _extend_spikes() -> void:
-	animation_player.play(SPIKES_EXTEND_ANIMATION_NAME)
-	spikes_down = false
-	
-	
-func _retract_spikes() -> void:
-	animation_player.play(SPIKES_RETRACT_ANIMATION_NAME)
-	spikes_down = true
-	
